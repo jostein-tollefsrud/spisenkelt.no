3b52f912f56d920b47a0576404f93ba3ff5b625e
Jostein Tollefsrud <jostein@selvklart.no>
2024-12-15 14:53:34 +0100

    chore: separate studio and site in app folder

---
+++ src/app/(studio)/layout.tsx
@@ -0,0 +1,15 @@
+import "../globals.css";
+
+export const dynamic = "force-static";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen">{children}</body>
+    </html>
+  );
+}
