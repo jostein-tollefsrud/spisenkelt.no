bf77a791aae466f78ecca0313e724bde6f3ea2a8
Jostein Tollefsrud <jostein@selvklart.no>
2024-12-25 20:58:19 +0100

    feat: add Footer component and integrate it into layout

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import clsx from 'clsx';
+
+export default function Footer() {
+	return (
+		<footer
+			className={clsx('bg-primary', 'text-primary-foreground', 'p-4', "text-center")}
+		>
+			spisenkelt.no
+		</footer>
+	);
+}
